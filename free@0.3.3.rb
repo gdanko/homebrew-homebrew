42352637c8951d2c11be583134789a0a5ca27d88
gdanko <Gary_Danko@intuit.com>
2024-12-16 17:38:40 -0800

    Add all the free formulae

---
+++ free@0.3.3.rb
@@ -0,0 +1,19 @@
+class FreeAT033 < Formula
+    desc "Display the amount of free and used memory in the system."
+    homepage "https://github.com/gdanko/free"
+    url "https://codeload.github.com/gdanko/free/tar.gz/refs/tags/v0.3.3"
+    sha256 "142eed3753a0c71c5d39fe0bb00055ceac7ab899e2f917cde550bce6b1b6c986"
+    version "0.3.3"
+
+    depends_on "go" => "1.12"
+
+    def install
+        system "make", "build"
+        bin.install "darwin/free" => "free"
+        man1.install "darwin/free.1" => "free.1"
+    end
+
+    def test
+        system "#{bin}/free", "--help"
+    end
+end
---
+++ free@0.3.4.rb
@@ -0,0 +1,19 @@
+class FreeAT034 < Formula
+    desc "Display the amount of free and used memory in the system."
+    homepage "https://github.com/gdanko/free"
+    url "https://codeload.github.com/gdanko/free/tar.gz/refs/tags/v0.3.4"
+    sha256 "90d72c842e2130daac099a9a81c94dd88a4f48c0e144f7ba87eabe8c166bf79e"
+    version "0.3.4"
+
+    depends_on "go" => "1.12"
+
+    def install
+        system "make", "build"
+        bin.install "bin/free" => "free"
+        man1.install "share/man/man1/free.1" => "free.1"
+    end
+
+    def test
+        system "#{bin}/free", "--help"
+    end
+end
