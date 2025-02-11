02f40ab3d6c3beecdf170bb593d90b42f5db0efd
gdanko <gdanko@protonmail.com>
2025-02-10 18:20:34 -0800

    Add pstree

---
+++ pstree.rb
@@ -0,0 +1,18 @@
+class Pstree < Formula
+    desc "Display a tree of processes."
+    homepage "https://github.com/gdanko/pstree"
+    url "https://github.com/gdanko/pstree/archive/refs/tags/v0.1.0.tar.gz"
+    sha256 "649139eb889ec80998b28bb1f66ce532431129f942fafb499a8a176313369d4d"
+    version "0.1.0"
+
+    depends_on "go" => "1.12"
+
+    def install
+        system "make", "build"
+        bin.install "bin/pstree" => "free"
+    end
+
+    def test
+        system "#{bin}/pstree", "--help"
+    end
+end
