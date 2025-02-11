303e0dccebbfa0826a1306c718bde9a69f9ad647
gdanko <gdanko@protonmail.com>
2025-02-11 11:58:28 -0800

    Add 0.2.0

--- pstree@0.2.0.rb
+++ pstree@0.2.0.rb
@@ -1,9 +1,9 @@
-class PstreeAT011 < Formula
+class PstreeAT020 < Formula
     desc "Display a tree of processes."
     homepage "https://github.com/gdanko/pstree"
-    url "https://github.com/gdanko/pstree/archive/refs/tags/v0.1.1.tar.gz"
-    sha256 "7467743f3dbe4d0c97f521d42c0c8bbeef7c339d815a20d310ed78bb386ac16c"
-    version "0.1.1"
+    url "https://github.com/gdanko/pstree/archive/refs/tags/v0.2.0.tar.gz"
+    sha256 "a28c1c1d10ab82839a0d522e03f3ae26c524f0deebaf2f4d03df57b78a6e6ac5"
+    version "0.2.0"
 
     depends_on "go" => "1.12"
 
