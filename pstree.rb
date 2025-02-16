83600af01a7f423c7b199348ae04ab44926701b6
gdanko <gdanko@protonmail.com>
2025-02-16 11:20:42 -0800

    Remove 0.5.4

--- pstree.rb
+++ pstree.rb
@@ -2,7 +2,7 @@ class Pstree < Formula
     desc "Display a tree of processes."
     homepage "https://github.com/gdanko/pstree"
     url "https://github.com/gdanko/pstree/archive/refs/tags/v0.5.4.tar.gz"
-    sha256 "ddfcbd8e7227ab6a82159ab6bc9787efd146ba8623a96412d3d7926aadb421f7"
+    sha256 "3f5c63e71c75c5f29b114a1927ab2a38dd7d093b0d20fa5ec3b0a9a6d9809894"
     version "0.5.4"
 
     depends_on "go" => "1.12"
@@ -10,7 +10,7 @@ class Pstree < Formula
     def install
         system "make", "build"
         bin.install "bin/pstree" => "pstree"
-        man1.install "share/man/man1/pstree.1.groff" => "pstree.1"
+        man1.install "share/man/man1/pstree.1" => "pstree.1"
     end
 
     def test
