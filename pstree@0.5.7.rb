e483e44d998d74652bf2deafbb82a8b22945de27
gdanko <gdanko@protonmail.com>
2025-02-16 18:59:50 -0800

    Add 0.5.7

--- pstree@0.5.7.rb
+++ pstree@0.5.7.rb
@@ -1,4 +1,4 @@
-class Pstree < Formula
+class PstreeAT057 < Formula
     desc "Display a tree of processes."
     homepage "https://github.com/gdanko/pstree"
     url "https://github.com/gdanko/pstree/archive/refs/tags/v0.5.7.tar.gz"
