101731809443dbeb200db8f598c5ef900d73d1e0
Gary Danko <gary.danko@servicenow.com>
2025-07-05 11:16:15 -0700

    Remove 0.8.0 again

--- pstree.rb
+++ pstree.rb
@@ -2,7 +2,7 @@ class Pstree < Formula
     desc "Display a tree of processes."
     homepage "https://github.com/gdanko/pstree"
     url "https://github.com/gdanko/pstree/archive/refs/tags/v0.8.0.tar.gz"
-    sha256 "3107e5d930b30f191596de9f1c7d432fe134f1e21674d89f775e28750c220f1f"
+    sha256 "4091e6c67ab5232ed9073ba4798a11139afdaa4316619f930bcbb902772b5cad"
     version "0.8.0"
 
     depends_on "go" => "1.12"
